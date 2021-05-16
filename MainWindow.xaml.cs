68ae3bfed5d8d09e63f9843d5d156d46a7a685be
dejan.subasic <dejan.subasic@gmail.com>
2021-05-16 22:21:20 +0200

    pokusaj rotacije

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -26,11 +27,14 @@ namespace PZ3
         private Point diffOffset = new Point();
         private int zoomMax = 30;
         private int zoomCurent = 1;
+        private bool middleMouseDown = false;
+        private Point middleClickPoint;
 
         public MainWindow()
         {
             InitializeComponent();
             _importer = new Importer();
+            //viewPortDisplay.RotateGesture = new MouseGesture(MouseAction.MiddleClick);
             //_importer.LoadModel();
         }
 
@@ -39,8 +43,8 @@ namespace PZ3
         {
             viewPortDisplay.CaptureMouse();
             start = e.GetPosition(this);
-            diffOffset.X = translacija.OffsetX;
-            diffOffset.Y = translacija.OffsetY;
+            diffOffset.X = trasnlation.OffsetX;
+            diffOffset.Y = trasnlation.OffsetY;
         }
 
         private void viewport1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
@@ -59,10 +63,26 @@ namespace PZ3
                 double h = this.Height;
                 double translateX = (offsetX * 100) / w;
                 double translateY = -(offsetY * 100) / h;
-                translacija.OffsetX = diffOffset.X + (translateX / (100 * skaliranje.ScaleX));
-                translacija.OffsetY = diffOffset.Y + (translateY / (100 * skaliranje.ScaleX));
+                trasnlation.OffsetX = diffOffset.X + (translateX / (100 * scale.ScaleX));
+                trasnlation.OffsetY = diffOffset.Y + (translateY / (100 * scale.ScaleX));
+
+            }
+
+            if (middleMouseDown)
+            {
+                Point mouse = e.GetPosition(this);
+                double diffX = mouse.X - middleClickPoint.X;
+                double diffY = mouse.Y - middleClickPoint.Y;
+
+                rotation.Axis = new Vector3D(diffY, diffX, 0);
+                rotation.Angle = Math.Sqrt(diffX * diffX + diffY * diffY) * 0.3;
+
+
+                Console.WriteLine("X" + diffX + " Y" + diffY);
+                
             }
         }
+
 
         private void viewport1_MouseWheel(object sender, MouseWheelEventArgs e)
         {
@@ -71,20 +91,77 @@ namespace PZ3
             double scaleY = 1;
             if (e.Delta > 0 && zoomCurent < zoomMax)
             {
-                scaleX = skaliranje.ScaleX + 0.1;
-                scaleY = skaliranje.ScaleY + 0.1;
+                scaleX = scale.ScaleX + 0.1;
+                scaleY = scale.ScaleY + 0.1;
                 zoomCurent++;
-                skaliranje.ScaleX = scaleX;
-                skaliranje.ScaleY = scaleY;
+                scale.ScaleX = scaleX;
+                scale.ScaleY = scaleY;
             }
             else if (e.Delta <= 0 && zoomCurent > -zoomMax)
             {
-                scaleX = skaliranje.ScaleX - 0.1;
-                scaleY = skaliranje.ScaleY - 0.1;
+                scaleX = scale.ScaleX - 0.1;
+                scaleY = scale.ScaleY - 0.1;
                 zoomCurent--;
-                skaliranje.ScaleX = scaleX;
-                skaliranje.ScaleY = scaleY;
+                scale.ScaleX = scaleX;
+                scale.ScaleY = scaleY;
             }
         }
+
+
+        private void AjustRoration(double offsetX, double offsetY)
+        {
+            if ((Math.Abs(offsetX) > Math.Abs(offsetY) && offsetX < 0))
+            {
+                rotation.Axis = new Vector3D(1, 0, 1);
+                rotation.Angle = rotation.Angle + 0.2;
+            }
+            if ((Math.Abs(offsetX) > Math.Abs(offsetY) && offsetX > 0))
+            {
+                rotation.Axis = new Vector3D(1, 1, 1);
+                rotation.Angle = rotation.Angle - 0.2;
+            }
+            if ((Math.Abs(offsetX) < Math.Abs(offsetY) && offsetY > 0))
+            {
+                if (rotation.Angle < 90)
+                {
+                    rotation.Axis = new Vector3D(1, 0, 1);
+                    rotation.Angle = rotation.Angle + 0.2;
+                }
+            }
+            if ((Math.Abs(offsetX) < Math.Abs(offsetY) && offsetY < 0))
+            {
+                if (rotation.Angle >= 0)
+                {
+                    rotation.Axis = new Vector3D(1, 1, 1);
+                    rotation.Angle = rotation.Angle - 0.2;
+                }
+            }
+        }
+
+        private void viewPortDisplay_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Middle)
+            {
+                middleMouseDown = true;
+                middleClickPoint = e.GetPosition(this);
+                Console.WriteLine(middleMouseDown);
+            }
+        }
+
+        private void viewPortDisplay_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Middle)
+            {
+                middleMouseDown = false;
+                Console.WriteLine(middleMouseDown);
+            }
+        }
+
+        private void viewPortDisplay_MouseLeave(object sender, MouseEventArgs e)
+        {
+            middleMouseDown = false;
+        }
+
+
     }
 }
