9fe41d585563e9f15c7b4cb86a8a2c2e2a5f539e
dejan.subasic <dejan.subasic@gmail.com>
2021-05-16 21:05:48 +0200

    zoom and pan

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,12 +22,69 @@ namespace PZ3
     public partial class MainWindow : Window
     {
         Importer _importer;
+        private Point start = new Point();
+        private Point diffOffset = new Point();
+        private int zoomMax = 30;
+        private int zoomCurent = 1;
 
         public MainWindow()
         {
             InitializeComponent();
             _importer = new Importer();
-            _importer.LoadModel();
+            //_importer.LoadModel();
+        }
+
+
+        private void viewport1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            viewPortDisplay.CaptureMouse();
+            start = e.GetPosition(this);
+            diffOffset.X = translacija.OffsetX;
+            diffOffset.Y = translacija.OffsetY;
+        }
+
+        private void viewport1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            viewPortDisplay.ReleaseMouseCapture();
+        }
+
+        private void viewport1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (viewPortDisplay.IsMouseCaptured)
+            {
+                Point end = e.GetPosition(this);
+                double offsetX = end.X - start.X;
+                double offsetY = end.Y - start.Y;
+                double w = this.Width;
+                double h = this.Height;
+                double translateX = (offsetX * 100) / w;
+                double translateY = -(offsetY * 100) / h;
+                translacija.OffsetX = diffOffset.X + (translateX / (100 * skaliranje.ScaleX));
+                translacija.OffsetY = diffOffset.Y + (translateY / (100 * skaliranje.ScaleX));
+            }
+        }
+
+        private void viewport1_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point p = e.MouseDevice.GetPosition(this);
+            double scaleX = 1;
+            double scaleY = 1;
+            if (e.Delta > 0 && zoomCurent < zoomMax)
+            {
+                scaleX = skaliranje.ScaleX + 0.1;
+                scaleY = skaliranje.ScaleY + 0.1;
+                zoomCurent++;
+                skaliranje.ScaleX = scaleX;
+                skaliranje.ScaleY = scaleY;
+            }
+            else if (e.Delta <= 0 && zoomCurent > -zoomMax)
+            {
+                scaleX = skaliranje.ScaleX - 0.1;
+                scaleY = skaliranje.ScaleY - 0.1;
+                zoomCurent--;
+                skaliranje.ScaleX = scaleX;
+                skaliranje.ScaleY = scaleY;
+            }
         }
     }
 }
